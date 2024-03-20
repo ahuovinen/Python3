5415804bd764feb362545127c83a8766f7192bb4
Antti Huovinen <ahuovinen@gmail.com>
2024-03-20 07:21:59 +0200

    modified:   calculator/calculator.py

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,20 +1,20 @@
 class calculator:
-    def __init__(self) -> None: # basic init method.
-        self.res()              # call reset total method.
-    def add(self, x):           # addition method
+    def __init__(self) -> None:     # basic init method.
+        self.res()                  # call reset total method.
+    def add(self, x):               # addition method
         self.Total += x
-    def sub(self, x):           # subtracktion method
+    def sub(self, x):               # subtraction method
         self.Total -= x
-    def div(self, x):           # division method
-        if x == 0:              # cant divide by zero
+    def div(self, x):               # division method
+        if x == 0:                  # cant divide by zero
             print("dumbass")
         else:
             self.Total /= x
-    def mul(self, x):           # multiplication method
+    def mul(self, x):               # multiplication method
         self.Total *= x
-    def res(self):              # reset calculator to 0.0
+    def res(self):                  # reset calculator to 0.0
         total = 0.0 
-    def getTotal(self):         # return calculator value
+    def getTotal(self) -> float:    # return calculator value
         return self.Total
      
 def main():
